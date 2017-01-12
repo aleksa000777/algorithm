e265f1927da52ff8c560c053fe03abf1a64b3d8d
Aleksandra Matiyev <Aleksandra.Matiyev@vitaminshoppe.com>
2017-01-12 14:17:48 +0000

    adds single number

---
+++ single_number.js
@@ -0,0 +1,12 @@
+// Given an array of integers, every element appears twice except for one. Find that single one.
+//
+// Note:
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+var singleNumber = function(nums) {
+    var result = 0;
+    for(var i=0; i<nums.length; i++){
+      result ^=nums[i];
+    }
+    return result
+};
