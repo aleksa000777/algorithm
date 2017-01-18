d804676137460ed1612d9c907697b84bfec384b8
Aleksandra Matiyev <aleksa000777@mail.ru>
2017-01-18 08:50:11 -0500

    Create assign_cookies.js

---
+++ assign_cookies.js
@@ -0,0 +1,23 @@
+// Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie. Each child i has a greed factor gi, which is the minimum size of a cookie that the child will be content with; and each cookie j has a size sj. If sj >= gi, we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
+// 
+// Note:
+// You may assume the greed factor is always positive. 
+// You cannot assign more than one cookie to one child.
+
+var findContentChildren = function(g, s) {
+  var result=0;
+  var x = g.sort(function(a, b){return a - b});;
+  var y = s.sort(function(a, b){return a - b});;
+  var count=0;
+  var temp=0;
+  var len = x.length;
+  for(var i =0;i<y.length;i++){
+    if(temp>=x.length) return result;
+    if(y[i]>=x[temp]) {
+      result++;
+      temp++
+    }
+  }
+  
+    return result
+};
